8f7e395c7fe00cac986b9e87cdece353b1e7b90c
Vivek Anand <vivekanand1101@gmail.com>
2019-07-09 17:11:50 +0530

    products: remove image field
    Signed-off-by: Vivek Anand <vivekanand1101@gmail.com>

--- src/products/models.py
+++ src/products/models.py
@@ -57,8 +57,6 @@ class Product(models.Model):
     slug = models.SlugField(blank=True, unique=True)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
-    image = models.ImageField(
-        upload_to=upload_image_path, null=True, blank=True)
     active = models.BooleanField(default=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     bandwidth = models.IntegerField(default=1)
